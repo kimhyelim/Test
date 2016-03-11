a2c79d2077d5d2068dba6ea6e12b4b696333834f
kimhyelim <limma0608@naver.com>
2016-03-11 11:55:20 +0900

    Test
    Test1

---
+++ Assets/06. Scripts/Test.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Test : MonoBehaviour {
+
+	public Rigidbody body;
+    public float speed = 150.0f;
+    public float rotSpeed = 30.0f;
+	public ForceMode mode;
+
+    void Start()
+    {
+
+       // body = GetComponent<Rigidbody>();
+
+    }
+    // Update is called once per frame
+    void FixedUpdate() {
+
+		Vector3 dir = transform.forward;
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            body.AddForce(dir * speed, mode);
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            body.AddForce(-dir * speed, mode);
+        }
+        if (Input.GetKey (KeyCode.A)) {
+			transform.Rotate(Vector3.up, -rotSpeed, Space.Self);
+
+		}
+		if (Input.GetKey (KeyCode.D)) {
+			transform.Rotate(Vector3.up, rotSpeed, Space.Self);
+		}
+
+
+	}
+}
---
+++ Assets/06. Scripts/testScrip.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class testScrip : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        this.gameObject.GetComponent<CharacterJoint>().connectedBody = transform.parent.GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+
+}
